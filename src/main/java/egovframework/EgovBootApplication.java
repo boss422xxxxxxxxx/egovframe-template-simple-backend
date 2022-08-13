4bd99c3f2fb4bbecab2b3218de2c90d0d5caa1de
boss422xxxxxxxxx <boss422@gmail.com>
2022-08-13 16:38:27 +0900

    EgovBootApplication

--- src/main/java/egovframework/EgovBootApplication.java
+++ src/main/java/egovframework/EgovBootApplication.java
@@ -13,13 +13,14 @@ import egovframework.com.config.EgovWebApplicationInitializer;
 @Import({EgovWebApplicationInitializer.class})
 public class EgovBootApplication {
 	public static void main(String[] args) {
-		System.out.println("##### EgovBootApplication Start #####");
+		System.out.println("##### EgovBootApplicationEgovBootApplication Start #####");
 
 		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
 		springApplication.setBannerMode(Banner.Mode.OFF);
 		//springApplication.setLogStartupInfo(false);
 		springApplication.run(args);
 
+		//재품ㄷ레9ㅐㅈㄷ레ㅐㅁ레ㅐㅣ;ㅣㅔㅇ레ㅣㅁㄷㄻㄹㄻㅁㄹㄷㅈ
 		System.out.println("##### EgovBootApplication End #####");
 	}
 }

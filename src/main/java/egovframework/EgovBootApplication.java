c6b1c6628ebf833ab70e212493f65c1fcbc1fb93
boss422xxxxxxxxx <boss422@gmail.com>
2022-08-13 22:12:12 +0900

    test

--- src/main/java/egovframework/EgovBootApplication.java
+++ src/main/java/egovframework/EgovBootApplication.java
@@ -1,12 +1,11 @@
 package egovframework;
 
+import egovframework.com.config.EgovWebApplicationInitializer;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.Import;
-
-import egovframework.com.config.EgovWebApplicationInitializer;
 
 @ServletComponentScan
 @SpringBootApplication
@@ -17,7 +16,7 @@ public class EgovBootApplication {
 
 		SpringApplication springApplication = new SpringApplication(EgovBootApplication.class);
 		springApplication.setBannerMode(Banner.Mode.OFF);
-		//springApplication.setLogStartupInfo(false);
+		////springApplication.setLogStartupInfo(false);
 		springApplication.run(args);
 
 		System.out.println("##### EgovBootApplication End #####");
